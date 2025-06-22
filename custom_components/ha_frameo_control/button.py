fd5cc982660671db4ab4bd6fde6dd990660a1e6e
Hunor Laczko <luni741@gmail.com>
2025-06-22 23:31:38 +0200

    fix buttons

--- custom_components/ha_frameo_control/button.py
+++ custom_components/ha_frameo_control/button.py
@@ -1,50 +1,51 @@
+"""Button entities for Frameo control."""
 from homeassistant.components.button import ButtonEntity, ButtonDeviceClass
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from .const import DOMAIN, CONF_CONN_TYPE, CONN_TYPE_USB, LOGGER
+
+from .const import DOMAIN, LOGGER
+from .api import FrameoAddonApiClient
 from . import FrameoConfigEntry
 
-async def async_setup_entry(hass: HomeAssistant, entry: FrameoConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+BUTTON_TYPES = {
+    "next_photo": {"name": "Next Photo", "command": "input swipe 800 500 100 500", "icon": "mdi:skip-next"},
+    "prev_photo": {"name": "Previous Photo", "command": "input swipe 100 500 800 500", "icon": "mdi:skip-previous"},
+    "pause_photo": {"name": "Pause Photo", "command": "input keyevent 85", "icon": "mdi:pause"},
+    "start_immich": {"name": "Start ImmichFrame", "command": "am start com.immichframe.immichframe/.MainActivity", "icon": "mdi:image-album"},
+    "start_frameo": {"name": "Start Frameo App", "command": "am start net.frameo.frame/.MainActivity", "icon": "mdi:image-multiple"},
+    "open_settings": {"name": "Open Settings", "command": "am start -a android.settings.SETTINGS", "icon": "mdi:cog"},
+    "start_wireless": {"name": "Start Wireless ADB", "command": "tcpip", "icon": "mdi:wifi-arrow-up-down", "device_class": ButtonDeviceClass.RESTART},
+}
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: FrameoConfigEntry, async_add_entities: AddEntitiesCallback):
     """Set up the Frameo button platform."""
-    client = entry.runtime_data
-    
-    entities = [
-        FrameoActionButton(client, "Next Photo", "next", "input swipe 800 500 100 500", entry, "mdi:skip-next"),
-        FrameoActionButton(client, "Previous Photo", "previous", "input swipe 100 500 800 500", entry, "mdi:skip-previous"),
-        FrameoActionButton(client, "Pause Photo", "pause", "input keyevent 85", entry, "mdi:pause"),
-        FrameoActionButton(client, "Start ImmichFrame", "start_immichframe", "am start com.immichframe.immichframe/.MainActivity", entry, "mdi:image-album"),
-        FrameoActionButton(client, "Start Frameo App", "start_frameo", "am start net.frameo.frame/.MainActivity", entry, "mdi:image-multiple"),
-        FrameoActionButton(client, "Open Settings", "open_settings", "am start -a android.settings.SETTINGS", entry, "mdi:cog"),
-    ]
-
-    if entry.data.get(CONF_CONN_TYPE) == CONN_TYPE_USB:
-        entities.append(FrameoStartWirelessAdbButton(client, entry))
-
+    client: FrameoAddonApiClient = entry.runtime_data
+    entities = [FrameoButton(client, entry, key, props) for key, props in BUTTON_TYPES.items()]
     async_add_entities(entities)
 
-class FrameoActionButton(ButtonEntity):
+
+class FrameoButton(ButtonEntity):
     _attr_has_entity_name = True
 
-    def __init__(self, client, name: str, endpoint: str, command: str, entry: FrameoConfigEntry, icon: str = None) -> None:
+    def __init__(self, client: FrameoAddonApiClient, entry: FrameoConfigEntry, key: str, props: dict):
         self.client = client
-        self._command = command
-        self._attr_name = name
-        self._attr_unique_id = f"{entry.entry_id}_{endpoint}"
-        self._attr_icon = icon
-        self._attr_device_info = {"identifiers": {(DOMAIN, entry.entry_id)}}
-    async def async_press(self) -> None:
+        self.key = key
+        self.props = props
+        self._attr_name = props["name"]
+        self._attr_unique_id = f"{entry.entry_id}_{key}"
+        self._attr_icon = props["icon"]
+        self._attr_device_class = props.get("device_class")
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, entry.entry_id)},
+            "name": entry.title,
+        }
+
+    async def async_press(self):
         LOGGER.error("Executing button '%s'", self.name)
-        await self.client.async_shell(self._command)
+        if self.key == "start_wireless":
+            await self.client.async_post_tcpip()
+        else:
+            await self.client.async_post_shell(self.props["command"])
 
-class FrameoStartWirelessAdbButton(ButtonEntity):
-    _attr_has_entity_name = True
-    _attr_device_class = ButtonDeviceClass.RESTART
-    _attr_name = "Start Wireless ADB"
-    def __init__(self, client, entry: FrameoConfigEntry) -> None:
-        self.client = client
-        self._attr_unique_id = f"{entry.entry_id}_start_wireless"
-        self._attr_device_info = {"identifiers": {(DOMAIN, entry.entry_id)}}
-    async def async_press(self) -> None:
-        LOGGER.error("Executing 'Start Wireless ADB' button.")
-        await self.client.async_tcpip(5555)
--- custom_components/ha_frameo_control/light.py
+++ custom_components/ha_frameo_control/light.py
@@ -1,14 +1,21 @@
+"""Light entity for Frameo screen control."""
 from homeassistant.components.light import LightEntity, ColorMode, ATTR_BRIGHTNESS
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
 from .const import DOMAIN, LOGGER
+from .api import FrameoAddonApiClient
 from . import FrameoConfigEntry
 
-async def async_setup_entry(hass: HomeAssistant, entry: FrameoConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: FrameoConfigEntry, async_add_entities: AddEntitiesCallback
+):
     """Set up the Frameo light platform."""
     client = entry.runtime_data
-    async_add_entities([FrameoScreen(client, entry)], update_before_add=True)
+    async_add_entities([FrameoScreen(client, entry)], True)
+
 
 class FrameoScreen(LightEntity):
     _attr_should_poll = True
@@ -16,58 +23,49 @@ class FrameoScreen(LightEntity):
     _attr_name = "Screen"
     _attr_supported_color_modes = {ColorMode.BRIGHTNESS}
 
-    def __init__(self, client, entry: FrameoConfigEntry) -> None:
+    def __init__(self, client: FrameoAddonApiClient, entry: FrameoConfigEntry):
         """Initialize the light."""
         self.client = client
         self._attr_unique_id = f"{entry.entry_id}_screen"
-        self._attr_device_info = {"identifiers": {(DOMAIN, entry.entry_id)}, "name": entry.title}
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, entry.entry_id)},
+            "name": entry.title,
+        }
         self._attr_is_on = None
         self._attr_brightness = None
+        self._attr_available = False
 
-    async def async_turn_on(self, **kwargs) -> None:
-        LOGGER.error("Turning on Frameo screen")
+    async def async_turn_on(self, **kwargs):
+        """Turn the screen on."""
+        LOGGER.error("Turning on Frameo screen for '%s'", self.name)
         if ATTR_BRIGHTNESS in kwargs:
             new_brightness = kwargs[ATTR_BRIGHTNESS]
             LOGGER.error("Setting Frameo brightness to %s", new_brightness)
-            await self.client.async_shell(f"settings put system screen_brightness {new_brightness}")
-            self._attr_brightness = new_brightness
-        
-        if self._attr_is_on is False:
-             await self.client.async_shell("input keyevent 26")
+            await self.client.async_post_shell(f"settings put system screen_brightness {new_brightness}")
+
+        state = await self.client.async_get_state()
+        if state and not state.get("is_on"):
+            await self.client.async_post_shell("input keyevent 26")
+
+        self.async_schedule_update_ha_state(True)
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the screen off."""
+        LOGGER.error("Turning off Frameo screen for '%s'", self.name)
+        state = await self.client.async_get_state()
+        if state and state.get("is_on"):
+            await self.client.async_post_shell("input keyevent 26")
+
+        self.async_schedule_update_ha_state(True)
+
+    async def async_update(self):
+        """Fetch the latest state from the device."""
+        LOGGER.error("Updating Frameo screen state for '%s'", self.name)
+        state = await self.client.async_get_state()
+        if state and "is_on" in state:
+            self._attr_is_on = state.get("is_on")
+            self._attr_brightness = state.get("brightness")
+            self._attr_available = True
         else:
-            power_state = await self.client.async_shell("dumpsys power")
-            if power_state and "mWakefulness=Asleep" in power_state:
-                await self.client.async_shell("input keyevent 26")
-
-        self._attr_is_on = True
-        self.async_write_ha_state()
-
-    async def async_turn_off(self, **kwargs) -> None:
-        LOGGER.error("Turning off Frameo screen")
-        if self._attr_is_on is True:
-            await self.client.async_shell("input keyevent 26")
-        else:
-            power_state = await self.client.async_shell("dumpsys power")
-            if power_state and "mWakefulness=Awake" in power_state:
-                await self.client.async_shell("input keyevent 26")
-
-        self._attr_is_on = False
-        self.async_write_ha_state()
-
-    async def async_update(self) -> None:
-        LOGGER.error("Updating Frameo screen state")
-        power_state = await self.client.async_shell("dumpsys power")
-        if not power_state:
             self._attr_available = False
-            return
 
-        self._attr_available = True
-        self._attr_is_on = "mWakefulness=Awake" in power_state
-        
-        for line in power_state.splitlines():
-            if "mScreenBrightnessSetting=" in line:
-                try:
-                    self._attr_brightness = int(line.split("=")[1])
-                    break
-                except (ValueError, IndexError):
-                    LOGGER.error("Could not parse brightness from line: %s", line)
