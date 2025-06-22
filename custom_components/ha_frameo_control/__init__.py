7cd6e5ac26d00ac800889f9a18af85a9035294bd
Hunor Laczko <luni741@gmail.com>
2025-06-23 00:16:41 +0200

    init fix

--- custom_components/ha_frameo_control/__init__.py
+++ custom_components/ha_frameo_control/__init__.py
@@ -1,24 +1,33 @@
 """The HA Frameo Control integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN, LOGGER, PLATFORMS
-from .api import FrameoAddonApiClient
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+from .api import FrameoAddonApiClient
+from .const import LOGGER, PLATFORMS
+
+# Define the type hint for the config entry
+type FrameoConfigEntry = ConfigEntry[FrameoAddonApiClient]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: FrameoConfigEntry) -> bool:
     """Set up HA Frameo Control from a config entry."""
-    api_client = FrameoAddonApiClient(hass)
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = api_client
-    
+    LOGGER.info("Setting up Frameo integration for %s", entry.title)
+
+    client = FrameoAddonApiClient(hass)
+    entry.runtime_data = client
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    entry.async_on_unload(entry.add_update_listener(async_update_listener))
     return True
+
 
 async def async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+
+async def async_unload_entry(hass: HomeAssistant, entry: FrameoConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    LOGGER.info("Unloading Frameo integration for %s", entry.title)
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
