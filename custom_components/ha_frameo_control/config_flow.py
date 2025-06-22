304733bfe72a92eb0595528e50d9ca345865d55e
Hunor Laczko <luni741@gmail.com>
2025-06-22 18:13:10 +0200

    fix timeout bug

--- custom_components/ha_frameo_control/config_flow.py
+++ custom_components/ha_frameo_control/config_flow.py
@@ -1,29 +1,47 @@
 import voluptuous as vol
+
 from adb_shell.adb_device import AdbDeviceUsb
 from adb_shell.adb_device_async import AdbDeviceTcpAsync
-from adb_shell.exceptions import AdbConnectionError, AdbTimeoutError, UsbDeviceNotFoundError, TcpTimeoutException
+from adb_shell.exceptions import (
+    AdbConnectionError,
+    AdbTimeoutError,
+    TcpTimeoutException,
+    UsbDeviceNotFoundError,
+)
 
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.const import CONF_HOST, CONF_PORT
-from .const import DOMAIN, CONF_CONN_TYPE, CONN_TYPE_NETWORK, CONN_TYPE_USB, CONF_SERIAL, LOGGER
+
+from .const import (
+    CONF_CONN_TYPE,
+    CONF_SERIAL,
+    CONN_TYPE_NETWORK,
+    CONN_TYPE_USB,
+    DOMAIN,
+    LOGGER,
+)
+
 
 async def _test_connection_usb(hass: HomeAssistant, serial: str | None) -> bool:
     """Test the USB ADB connection in an executor job."""
+
     def test_sync_usb():
         """Synchronous USB test."""
         LOGGER.error("Testing synchronous USB connection with serial: %s", serial)
         device = AdbDeviceUsb(serial=serial)
-        device.connect(timeout_s=5.0)
+        # The connect call for the synchronous library is blocking.
+        device.connect(transport_timeout_s=5.0)
         device.close()
         LOGGER.error("Synchronous USB connection test successful.")
         return True
-    
+
     return await hass.async_add_executor_job(test_sync_usb)
 
 
 class HaFrameoControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for HA Frameo Control."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
@@ -44,12 +62,18 @@ class HaFrameoControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             self._abort_if_unique_id_configured()
 
             try:
-                LOGGER.error("Testing network connection to %s:%s", user_input[CONF_HOST], user_input[CONF_PORT])
-                device = AdbDeviceTcpAsync(host=user_input[CONF_HOST], port=user_input[CONF_PORT])
-                await device.connect(timeout_s=5.0)
+                LOGGER.error(
+                    "Testing network connection to %s:%s",
+                    user_input[CONF_HOST],
+                    user_input[CONF_PORT],
+                )
+                device = AdbDeviceTcpAsync(
+                    host=user_input[CONF_HOST], port=user_input[CONF_PORT]
+                )
+                await device.connect(transport_timeout_s=5.0)
                 await device.close()
                 LOGGER.error("Network connection test successful")
-                
+
                 return self.async_create_entry(
                     title=f"Frameo ({user_input[CONF_HOST]})",
                     data={CONF_CONN_TYPE: CONN_TYPE_NETWORK, **user_input},
